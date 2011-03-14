3ca631a464b5d6933ffb27afd49cb1ab7de8fe29
fearphage <fearphage@gmail.com>
2011-03-14 13:38:21 -0500

    intercept link clicks that should be sent to the phone (like firefox extension) directory reorganization code cleanup and optimization

--- includes/operatophone.js
+++ includes/operatophone.js
@@ -3,80 +3,87 @@
 // @include https://*
 // ==/UserScript==
 
-(function(window) {
-if ( window != window.top) { return; }
-	
-	var ACTION_CAPTURE_SELECTION = 'capture_selection';
-	var ACTION_SEND_PAGE = 'send_to_phone';
-	var ACTION_CLOSE_TAB = 'close_tab';
-	
-	var currentUrl = document.location.href;
-	
-	opera.extension.addEventListener( 'message', function( message ) {
-		if( message.data.action === ACTION_CAPTURE_SELECTION ) {
-			var pageInfo = {
-				action: ACTION_SEND_PAGE,
-				data: {
-					link: currentUrl,
-					title: document.title,
-					selection: window.getSelection() ? window.getSelection().toString() : null
-				}
-			};
-			// URL overrides
-			if ( currentUrl.match( /^http[s]?:\/\/maps\.google\./i ) ||
-					currentUrl.match( /^http[s]?:\/\/www\.google\.[a-z]{2,3}(\.[a-z]{2})\/maps/i ) ) {
-			  var link = document.getElementById('link');
-			  if (link && link.href)
-				  pageInfo.data.link = link.href;
-			}
-			opera.extension.postMessage( pageInfo );
-		}
-	}, false);
-	
-	function findAndReplace(searchText, replacement, searchNode) {
-	    if (!searchText || typeof replacement === 'undefined') {
-	        // Throw error here if you want...
-	        return;
-	    }
-	    var regex = typeof searchText === 'string' ?
-	                new RegExp(searchText, 'g') : searchText,
-	        childNodes = (searchNode || document.body).childNodes,
-	        cnLength = childNodes.length,
-	        excludes = 'html,head,style,title,link,meta,script,object,iframe';
-	    while (cnLength--) {
-	        var currentNode = childNodes[cnLength];
-	        if (currentNode.nodeType === 1 &&
-	            (excludes + ',').indexOf(currentNode.nodeName.toLowerCase() + ',') === -1) {
-	            arguments.callee(searchText, replacement, currentNode);
-	        }
-	        if (currentNode.nodeType !== 3 || !regex.test(currentNode.data) ) {
-	            continue;
-	        }
-	        var parent = currentNode.parentNode,
-	            frag = (function(){
-	                var html = currentNode.data.replace(regex, replacement),
-	                    wrap = document.createElement('div'),
-	                    frag = document.createDocumentFragment();
-	                wrap.innerHTML = html;
-	                while (wrap.firstChild) {
-	                    frag.appendChild(wrap.firstChild);
-	                }
-	                return frag;
-	            })();
-	        parent.insertBefore(frag, currentNode);
-	        parent.removeChild(currentNode);
-	    }
-	}
-	window.addEventListener( 'DOMContentLoaded', function() {
-		if( currentUrl.match( /^http[s]?:\/\/www\.google\.com\/accounts\/ServiceLogin\?(.*)?ahname=Chrome\+to\+Phone(.*)?$/i ) ) {
-			// Opera log in message so users know what they are logging in to.
-			findAndReplace('Chrome', 'Opera', document.body); 
-		} else if (currentUrl.match( /^http:\/\/code\.google\.com\/p\/chrometophone\/logo(.*)?$/i )) {
-			opera.extension.postMessage({
-				action: ACTION_CLOSE_TAB
-			});
-		}
-	}, false);
-
-}
-})(this);
+window == window.top && (currentUrl = document.location.href) && (function(window, loc, document, opera, extension) {
+//  alert([window.top, window.parent, window.self, window == this.top,this.top,this.parent])
+//  if ( window != top) { return; }
+//  opera.postError('in here');  
+  var
+    ACTION_CAPTURE_SELECTION = 'capture_selection'
+    ,ACTION_SEND_PAGE = 'send_to_phone'
+    ,ACTION_CLOSE_TAB = 'close_tab'
+    ,forEach = Array.forEach || (function(fe) {
+      return function(array, fn, that) {
+        fe.call(array, fn, that);
+      };
+    })([].forEach)
+    ,validURI = /^(?:https?|market|tel|sms(?:to)?|mailto|ftp):/i
+    ,phoneURI = /^(?:market|tel|sms(?:to)?):|^http:\/\/chart\.apis\.google\.com\/chart\?.*?&?chl=([^&]+)/i
+    ,currentUrl = document.location.href
+  ;
+  
+  opera.extension.addEventListener( 'message', function( message ) {
+    if( message.data.action === ACTION_CAPTURE_SELECTION ) {
+      var pageInfo = {
+        action: ACTION_SEND_PAGE,
+        data: {
+          link: currentUrl,
+          title: document.title,
+          selection: document.getSelection() && document.getSelection().toString() || null
+        }
+      };
+      // URL overrides
+      if ( currentUrl.match( /^http[s]?:\/\/maps\.google\./i ) ||
+          currentUrl.match( /^http[s]?:\/\/www\.google\.[a-z]{2,3}(\.[a-z]{2})\/maps/i ) ) {
+        var link = document.getElementById('link');
+        if (link && link.href)
+          pageInfo.data.link = link.href;
+      }
+      opera.extension.postMessage( pageInfo );
+    }
+  }, false);
+  
+  function findAndReplace(searchText, replacement, parent) {
+      if (!searchText || typeof replacement === 'undefined') {
+          // Throw error here if you want...
+          return;
+      }
+      var regex = typeof searchText === 'string'
+        ? new RegExp(searchText, 'g')
+        : searchText
+      ;
+      forEach((parent || document.body).selectNodes('//text()[contains(., "' + searchText + '")]'), function(node) {
+        node.data.replace(regex, replacement);
+      });
+  }
+  addEventListener( 'DOMContentLoaded', function() {
+    if(/^https?:\/\/www\.google\.com\/accounts\/ServiceLogin\?(.*)?ahname=Chrome\+to\+Phone(.*)?$/i.test(currentUrl)) {
+      // Opera log in message so users know what they are logging in to.
+      findAndReplace('Chrome', 'Opera', document.body); 
+    } else if (/^http:\/\/code\.google\.com\/p\/chrometophone\/logo(.*)?$/i.test(currentUrl)) {
+      opera.extension.postMessage({
+        action: ACTION_CLOSE_TAB
+      });
+    }
+  }, false);
+  document.addEventListener('click'
+    ,function(e, link) {
+      if (!(link = e.target.selectSingleNode('ancestor-or-self::a')) || !phoneURI.test(link.href)) { return; }
+      // todo: show some indication that the information has been sent to the phone instead
+      e.preventDefault();
+      var
+        match = link.href.match(phoneURI)
+        ,info = {
+          action: ACTION_SEND_PAGE
+          ,data: {
+            link: match && match[1] || link.href
+            ,title: link.innerText || link.getAttribute('title') || document.title
+            ,selection: document.getSelection() && document.getSelection().toString() || null
+          }
+        }
+      ;
+      opera.postError('chrome2phone - SENDING!\n' + JSON.stringify(info));
+      opera.extension.postMessage(info);
+    }
+    ,false
+  );
+})(this, this.location, this.document, this.opera, this.opera.extension);
